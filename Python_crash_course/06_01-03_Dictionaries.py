c612154db85f02be938dc7af3f0f2574e822e609
Björn Olsson <olsson.bjoern@gmail.com>
2026-01-25 21:30:09 +0100

    started crash course exercises

---
+++ Python_crash_course/06_01-03_Dictionaries.py
@@ -0,0 +1,40 @@
+#
+# 6.1 - Create dictionary of a person
+#
+
+
+ironMan = {
+    'first name': 'Tony',
+    'last name': 'Stark',
+    'power': 'Super smart, iron man suit',
+    'age' : '30-ish',
+    'city': 'New York'
+}
+
+print(ironMan['city'])
+print(ironMan['first name'])
+print(ironMan['last name'])
+print(ironMan['power'])
+
+# 6.2 Favorite numbers - store favorite numbers
+
+favNumbers = {
+    'karen':'3',
+    'steve':'16',
+    'björn':'4',
+    'jackie':'2',
+    'nelli':'9',
+}
+
+print(favNumbers['karen'])
+print(favNumbers['jackie'])
+print(favNumbers['nelli'])
+
+# 6.3 - Make a glossary
+
+glossary = {
+    'byte':'memory size',
+    'dictionary':'way of storing data',
+    'stoic':'silent, emotionless',
+    'traceback':'showing error message'
+}
