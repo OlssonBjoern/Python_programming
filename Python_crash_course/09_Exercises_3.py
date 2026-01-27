54302b5774582c04192d4769fd10557e7ec39587
Björn Olsson <olsson.bjoern@gmail.com>
2026-01-27 21:17:28 +0100

    Finished exercises

--- Python_crash_course/09_Exercises_3.py
+++ Python_crash_course/09_Exercises_3.py
@@ -19,7 +19,46 @@ class Restaurant:
     def describe_restaurant(self):
         print(f"This restaurant serves {self.cuisine}.")
 
+class IceCreamStand(Restaurant):
+
+    def __init__(self, restaurant_name, cuisine, flavours):
+        super().__init__(restaurant_name, cuisine)
+        self.flavours = flavours
+
+    def displayFlavours(self):
+        print("Current flavours to chose from: ")
+        for flavour in self.flavours:
+            print(flavour)
+
+stand = IceCreamStand("Boms", "Ice cream", ["Strawberry", "Vanilla", "Pistachio"])
+
+stand.displayFlavours()
+
 
 # 9.7 Admin
+# Extension of old User class, should have privileges attribute and a method called "show_privileges()"
+
+class User:
+
+    def __init__(self, name, role, userId):
+        self.name = name
+        self.role = role
+        self.userId = userId
+    
+
+
+class Admin(User):
+
+    def __init__(self, name, role, userId, privileges):
+        super().__init__(name, role, userId)
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print(f"{self.name} works in {self.role} with ID: {self.userId} and has admin level: {self.privileges[1]}")
+
+
+ad = Admin("Steve Jobs", "Idiot branch", 3, ["can post", "full privileges", "can't do shit"])
+
+ad.show_privileges()
 
 # 9.8 Privileges
