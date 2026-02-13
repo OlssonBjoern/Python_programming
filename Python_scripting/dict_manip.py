97a5fcc853792ceb9594e33c5736629163b6a445
Olsson <Bjoern.Olsson@phonak.com>
2026-02-13 22:30:42 +0100

    started with list manipulation for future Python Scripting

--- Python_scripting/dict_manip.py
+++ Python_scripting/dict_manip.py
@@ -35,8 +35,17 @@ print(unique_teams2)
 list_unique_years = []
 
 for data in comicchardict:
+    # Each "data" is every dictionary in the list of dictionaries
     yor = data["year_of_release"]
     if yor not in list_unique_years:
         list_unique_years.append(yor)
 
+unique_teams3 = []
+for data in comicchardict:
+    if data["teams"] in unique_teams3:
+        continue
+    else:
+        unique_teams3.append(data["teams"])
+
 print(list_unique_years)
+print(unique_teams3)
